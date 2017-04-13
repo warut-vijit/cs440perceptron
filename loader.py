cf84e3232c306b16de9e3451e3f82bf5746cb9f5
Duke Vijitbenjaronk <warut.vijitbenjaronk@gmail.com>
2017-04-12 20:32:05 -0500

    added mnist file reader

---
+++ loader.py
@@ -0,0 +1,33 @@
+import pickle
+import numpy as np
+
+def load_mnist_data(file_name, offset=0, batch_max=10):
+    """
+        Takes a MNIST file as input, with offset (in examples) and max batch size. 
+        Returns: 2d numpy array of shape (batch_max, 784)
+        Throws: 
+    """
+    objects = [] # examples
+    try:
+        with (open(file_name, "r")) as openfile:
+            openfile.seek(offset * 840)
+            batch = 0
+            end = False
+            while True:
+                try:
+                    obj = np.array([])
+                    for _ in range(28):
+                        raw_line = openfile.readline()
+                        processed_line = [(x=='#')+(x=='+')/2.0+0.0 for x in raw_line if x!='\n']
+                        obj = np.concatenate((obj, processed_line))
+                    objects.append(obj)
+                    batch += 1 # count items in batch
+                    if batch == batch_max:
+                        break
+                except EOFError:
+                    end=True
+                    break
+            ptr = openfile.tell()/840 # record position in file
+        return {'data': np.vstack(objects), 'offset':ptr, 'end':end}
+    except IOError:
+        raise IOError("Unable to open file %s." % file_name)

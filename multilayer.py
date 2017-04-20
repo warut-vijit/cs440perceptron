dbc2a7095ba386c6fa8633ddbdbb8a08fb3e5d17
Duke Vijitbenjaronk <warut.vijitbenjaronk@gmail.com>
2017-04-20 18:28:50 -0500

    finished deep neural network

--- multilayer.py
+++ multilayer.py
@@ -2,13 +2,15 @@ from loader import load_mnist_data, load_mnist_labels
 import csv
 import logging
 import numpy as np
-import math
 import sys
+import random
+from matplotlib import pyplot
 
-LEARNING_RATE = 0.05
-EPOCHS = 3000
-REPORT = 150
+LEARNING_RATE = 0.001
+EPOCHS = 30000
+REPORT = 100
 TEST_BATCH_SIZE = 100
+TRAIN_BATCH_SIZE = 100
 DROPOUT_P = 0.5
 
 # Helper functions for output squashing and backprop
@@ -24,7 +26,7 @@ class NN_Layer:
 
     def __init__(self, in_size, out_size):
         # Initialize trainable parameters
-        self.weights = np.matrix(np.random.randn(in_size + 1, out_size) * 0.05)
+        self.weights = np.array(np.random.randn(in_size + 1, out_size) * 0.05)
         self.input = None # for convenience, store input to this layer
         self.output = None # for convenience, store output from this layer
 
@@ -54,31 +56,31 @@ class NN_Network:
             error = np.dot(delta, layer.weights.T)[:,:-1]
             layer.weights += np.dot(layer.input.T, delta) * LEARNING_RATE
 
-    def train(self, arr, epochs):
+    def train(self, examples, labels, epochs):
         """Takes a 2D array of floats as input, formatted with last item as labels and all others as attributes"""
         for x in range(epochs):
-            forward = self.feed_forward(np.matrix(arr[:,:-1]))
-            self.backprop(forward,arr[:,-1:])
-            if x%REPORT == 0:
-                logging.warning("fin l'epoch "+str(x))
-
-    def test(self, arr):
-        ff_result = self.feed_forward(arr[:,:-1])
-        for inx in xrange(len(ff_result)):
-            logging.error(str(ff_result[inx])+" : "+str(arr[inx,-1]))
-        sum_error = np.sum(abs(ff_result-arr[:,-1:]))
-        return sum_error/len(arr)
+            forward = self.feed_forward(examples)
+            self.backprop(forward, labels)
 
 if __name__ == "__main__":
-        # Do 20% testing and 80% training by default
-        # get test data
-        train_input = load_mnist_data('trainingimages', offset=0, batch_max=TEST_BATCH_SIZE)['data']
-        train_label = load_mnist_labels('traininglabels', offset=0, batch_max=TEST_BATCH_SIZE)['data']
-        data = np.concatenate((train_input, train_label), axis=1)
-        if int(sys.argv[1]) != len(data[0])-1:
-            raise ValueError("\033[91mInput layer ("+sys.argv[1]+") and data ("+str(len(data[0])-1)+") do not match. Terminating...\033[0m")
-        training = data[:4*len(data)/5]
-        test = data[4*len(data)/5:]
-        neural_net = NN_Network([int(arg) for arg in sys.argv[1:]])
-        neural_net.train(training, EPOCHS)
-        print "\nAverage error on test is: \033[91m" + str(neural_net.test(test))
+    # Do 20% testing and 80% training by default
+    # get test data and train
+    results = []
+    neural_net = NN_Network([int(arg) for arg in sys.argv[1:]])
+    for k in xrange(EPOCHS / REPORT):
+        offset = random.randint(0, 1000-TRAIN_BATCH_SIZE)
+        train_input = load_mnist_data('trainingimages', offset=offset, batch_max=TRAIN_BATCH_SIZE)['data']
+        train_label = load_mnist_labels('traininglabels', offset=offset, batch_max=TRAIN_BATCH_SIZE)['data'] # get label
+        train_label = np.array([[x==y[0] for x in xrange(10)] for y in train_label])
+        neural_net.train(train_input, train_label, REPORT)
+        logging.error("Finished epoch %s" % (k * REPORT))
+        # test
+        test_input = load_mnist_data('testimages', offset=0, batch_max=TEST_BATCH_SIZE)['data']
+        test_label = load_mnist_labels('testlabels', offset=0, batch_max=TEST_BATCH_SIZE)['data']
+        test_sum = 0
+        labels = neural_net.feed_forward(test_input)
+        for index in xrange(len(test_label)):
+            test_sum += (max(enumerate(labels[index]), key=lambda b:b[1])[0] == test_label[index])
+        results.append(float(test_sum)/TEST_BATCH_SIZE)
+    pyplot.plot(results)
+    pyplot.show()
